6c638d49d0277c8403b7ee96138f218935803d24
udaysuryablr <107272845+udaysuryablr@users.noreply.github.com>
2024-07-07 23:17:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.layout import create_layout
 from dash_bootstrap_components.themes import BOOTSTRAP
 
 app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
-server = app.server
+server = app.server  # Exposing the server for Gunicorn
 app.title = "Walmart Project"
 app.layout = create_layout(app)
 
